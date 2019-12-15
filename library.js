c5854234cc2abad8df83c390875eaf1113d3d7b1
Victor Zhang <vcz.zhang@gmail.com>
2019-12-14 17:16:13 -0800

    JS WIRED UP :D

---
+++ library.js
@@ -0,0 +1,53 @@
+let myLibrary = [
+    new Book('J.R.R. Tolkien', 'The Hobbit', '200', '1')
+];
+
+function Book(author, title, numOfPages, isRead) {
+  // the constructor...
+  this.author = author;
+  this.title = title;
+  this.numOfPages = numOfPages;
+  this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+    let author = document.getElementById('author').value;
+    let title = document.getElementById('title').value;
+    let numOfPages = document.getElementById('page-count').value;
+    let isRead = document.querySelector('input[name="isRead"]:checked').value;
+
+    let newBook = new Book(author, title, numOfPages, isRead);
+
+    myLibrary.push(newBook);
+    render();
+}
+
+function render() {
+    // displays each book on the page
+    document.getElementById('table-body').innerHTML = '';
+
+    for (i=0; i < myLibrary.length; i++) {
+        var table = document.getElementById('table-body');
+        var row = table.insertRow();
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+
+        cell1.innerHTML = myLibrary[i].author;
+        cell2.innerHTML = myLibrary[i].title;
+        cell3.innerHTML = myLibrary[i].numOfPages;
+        cell4.innerHTML = myLibrary[i].isRead;
+    }
+}
+
+function openForm() {
+    document.getElementById('book-form').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('book-form').style.display = 'none';
+}
+
+render();
